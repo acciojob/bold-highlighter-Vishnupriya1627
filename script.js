f4a02183c1c81d3ddbb96ba88296739c1f362c56
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-06-01 18:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let strong = document.querySelectorAll("strong");
+	strong.forEach((element)=>{
+		element.style.color = "rgb(0, 128, 0)"
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let elements = document.querySelectorAll("strong");
+	elements.forEach((element)=>{
+		element.style.color = "rgb(0, 0, 0)"
+	})
     
 }
